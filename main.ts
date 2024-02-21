2753bbf404784ac11edba5dc9971658123f3fc18
benschoern <145876175+benschoern@users.noreply.github.com>
2024-02-21 18:09:22 +0100

    init

--- main.ts
+++ main.ts
@@ -1,1 +1,41 @@
-
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    for (let y = 0; y <= 4; y++) {
+        for (let x = 0; x <= 4; x++) {
+            led.plot(x, y)
+            basic.pause(25)
+        }
+    }
+    for (let x = 0; x <= 4; x++) {
+        for (let y = 0; y <= 4; y++) {
+            led.unplot(x, y)
+            basic.pause(25)
+        }
+    }
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    for (let y = 0; y <= 4; y++) {
+        for (let x = 0; x <= 4; x++) {
+            led.plot(y, x)
+            basic.pause(25)
+        }
+    }
+    for (let x = 0; x <= 4; x++) {
+        for (let y = 0; y <= 4; y++) {
+            led.unplot(y, x)
+            basic.pause(25)
+        }
+    }
+})
+for (let index = 0; index < 3; index++) {
+    basic.showIcon(IconNames.SmallHeart, 100)
+    basic.setLedColor(0xff0000)
+    basic.showIcon(IconNames.Heart, 100)
+    basic.turnRgbLedOff()
+}
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
